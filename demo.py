eaae734a2459ef779939ede01017dccc947ce05a
Samuel Rivera <samuel.rivera@matrixresearch.com>
2021-02-06 13:05:17 -0500

    credits and more info

--- demo.py
+++ demo.py
@@ -1,3 +1,6 @@
+# Notes: basic yfinance demo mostly taken from:
+# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+
 import yfinance as yf
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -17,7 +20,18 @@ tickerDf = tickerData.history(period='1d', start='2010-1-1', end='2020-1-25')
 # see your data
 tickerDf
 
-
+# make a nice little figure
 print(tickerDf.columns)
 sns.lineplot(x='Date', y='Close', data=tickerDf)
 plt.title(tickerSymbol)
+
+
+# ---------
+# info on the company
+tickerData.info
+
+# get event data for ticker
+tickerData.calendar
+
+# get recommendation data for ticker
+tickerData.recommendations
