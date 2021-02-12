0e620a79669e43f28acdc2c36b82c373833f85ce
Samuel Rivera <samuel.rivera@matrixresearch.com>
2021-02-12 17:14:05 -0500

    refactor for importing data cleanly and making requirements

--- models/features.py
+++ models/features.py
@@ -2,12 +2,8 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from data import Ticker2
 import os
-
-figDir = 'Figs'
-sns.set_theme(style='darkgrid', font_scale=1.5)
-
+from data import Ticker2
 
 tickerSymbol = 'MSFT'
 tickerData = Ticker2(tickerSymbol)
@@ -16,16 +12,7 @@ tickerDf = tickerData.history(period='1d', start='2010-1-1', end='2020-1-25')
 
 # make Data a value you can work with
 tickerDf.reset_index('Date', inplace=True)
-# print(tickerDf.columns)
-print(tickerDf.info())
 
-
-# # make a nice little figure
-# sns.lineplot(x='Date', y='Close', data=tickerDf)
-# plt.title(tickerSymbol)
-
-tickerDf.columns
-tickerDf.head()
 
 # -----
 # features parameters
@@ -42,20 +29,6 @@ tickerDf['DailyChangeMean'] = tickerDf.rolling(
 tickerDf['DailyChangeStd'] = tickerDf.rolling(
     f"{dailyChangeRollAvgInterval_D}D", on='Date')['DailyChange'].std()
 
-# show it
-plt.figure(figsize=(6, 8))
-plt.subplot(2, 1, 1)
-# sns.barplot(x='Date', y='DailyChangeMean', data=tickerDf)
-ax = sns.scatterplot(x='Date', y='DailyChangeMean', data=tickerDf)
-ax.axhline(0, linestyle='-', color='red')
-ax.set_xticklabels([])
-ax.set_xlabel('')
-ax.set_ylabel(f'[C-O] Mean (Roll {dailyChangeRollAvgInterval_D} D)')
-
-plt.subplot(2, 1, 2)
-ax = sns.scatterplot(x='Date', y='DailyChangeStd', data=tickerDf)
-ax.set_ylabel(f'[C-O] STD (Roll {dailyChangeRollAvgInterval_D} D)')
-
 
 # ------
 # closing mean features ( for estimating good/bad at a delay)
@@ -63,11 +36,6 @@ ax.set_ylabel(f'[C-O] STD (Roll {dailyChangeRollAvgInterval_D} D)')
 tickerDf['CloseAvg'] = tickerDf.rolling(
     f"{closingRollAvgInterval_D}D", on='Date')['Close'].mean()
 
-# show closing average rolling mean
-plt.figure()
-sns.lineplot(x='Date', y='value', hue='variable',
-             data=pd.melt(tickerDf, id_vars=['Date'], value_vars=['Close', 'CloseAvg']))
-plt.title(tickerSymbol)
 
 # -------------------------
 # Time delay of moving average closing
@@ -83,51 +51,6 @@ tickerDf['Good'] = 0  # preset all bad
 tickerDf.loc[tickerDf['CloseFutureChange'] > 0, 'Good'] = 1
 
 
-plt.figure(figsize=(6, 8))
-plt.subplot(3, 1, 1)
-# plt.plot(tickerDf['Date'], tickerDf['Close'], '-')
-ax = sns.lineplot(x='Date', y='Close', data=tickerDf)
-ax.set_xticklabels([])
-ax.set_xlabel('')
-
-plt.subplot(3, 1, 2)
-# plt.plot(tickerDf['Date'], tickerDf['CloseFutureChange'], '.')
-ax = sns.scatterplot(x='Date', y='CloseFutureChange', data=tickerDf)
-ax.axhline(0, linestyle='-', color='red')
-ax.set_xticklabels([])
-ax.set_xlabel('')
-plt.ylabel(f'{-1*delay} day change')
-
-plt.subplot(3, 1, 3)
-# plt.plot(tickerDf['Date'], tickerDf['Good'], '.')
-# ax = sns.scatterplot(x='Date', y='Good', data=tickerDf, markers='+')
-kwargs = {"alpha": 1.0, "s": 2}  # "color": "darkred",
-ax = sns.scatterplot(x='Date', y='Good', data=tickerDf, **kwargs)
-plt.yticks([0, 1], ["No", "Yes"])
-plt.suptitle(tickerSymbol)
-# tickerDf
-# tickerDf.dropna(inplace=True)
-
-
-# -------------------------
-# show some relationships between variables of interest
-
-# print(tickerDf.columns)
-
-colsOfInterest = ['Volume', 'DailyChange', 'DailyChangeMean',
-                  'DailyChangeStd', 'CloseFutureChange', 'Good']
-
-sns.pairplot(tickerDf[colsOfInterest])
-
-# ---------------------------
-# Visualize the good and bad by 2 features
-plt.figure()
-
-sns.scatterplot(x='DailyChangeMean', y='DailyChangeStd',
-                hue='Good', data=tickerDf)
-plt.title(tickerSymbol)
-
-
 # df = tickerDf.groupby([pd.Grouper(key='Date', freq='W-MON')]
 #                       )  # weekly, mondays
 
@@ -140,3 +63,76 @@ plt.title(tickerSymbol)
 
 
 # subdf
+if __name__ == '__main__':
+
+    figDir = 'Figs'
+    sns.set_theme(style='darkgrid', font_scale=1.5)
+
+    # print(tickerDf.columns)
+    print(tickerDf.info())
+
+    # # make a nice little figure
+    # sns.lineplot(x='Date', y='Close', data=tickerDf)
+    # plt.title(tickerSymbol)
+
+    print(tickerDf.columns)
+
+    # ------------------
+    # show it daily change mean and stev
+    plt.figure(figsize=(6, 8))
+    plt.subplot(2, 1, 1)
+    ax = sns.scatterplot(x='Date', y='DailyChangeMean', data=tickerDf)
+    ax.axhline(0, linestyle='-', color='red')
+    ax.set_xticklabels([])
+    ax.set_xlabel('')
+    ax.set_ylabel(f'[C-O] Mean (Roll {dailyChangeRollAvgInterval_D} D)')
+    plt.subplot(2, 1, 2)
+    ax = sns.scatterplot(x='Date', y='DailyChangeStd', data=tickerDf)
+    ax.set_ylabel(f'[C-O] STD (Roll {dailyChangeRollAvgInterval_D} D)')
+
+    # show closing average rolling mean
+    plt.figure()
+    sns.lineplot(x='Date', y='value', hue='variable',
+                 data=pd.melt(tickerDf, id_vars=['Date'], value_vars=['Close', 'CloseAvg']))
+    plt.title(tickerSymbol)
+
+    # ------------------
+    # plot close, X day change, Good
+    plt.figure(figsize=(6, 8))
+    plt.subplot(3, 1, 1)
+    # plt.plot(tickerDf['Date'], tickerDf['Close'], '-')
+    ax = sns.lineplot(x='Date', y='Close', data=tickerDf)
+    ax.set_xticklabels([])
+    ax.set_xlabel('')
+
+    plt.subplot(3, 1, 2)
+    # plt.plot(tickerDf['Date'], tickerDf['CloseFutureChange'], '.')
+    ax = sns.scatterplot(x='Date', y='CloseFutureChange', data=tickerDf)
+    ax.axhline(0, linestyle='-', color='red')
+    ax.set_xticklabels([])
+    ax.set_xlabel('')
+    plt.ylabel(f'{-1*delay} day change')
+
+    plt.subplot(3, 1, 3)
+    # plt.plot(tickerDf['Date'], tickerDf['Good'], '.')
+    # ax = sns.scatterplot(x='Date', y='Good', data=tickerDf, markers='+')
+    kwargs = {"alpha": 1.0, "s": 2}  # "color": "darkred",
+    ax = sns.scatterplot(x='Date', y='Good', data=tickerDf, **kwargs)
+    plt.yticks([0, 1], ["No", "Yes"])
+    plt.suptitle(tickerSymbol)
+    # tickerDf
+    # tickerDf.dropna(inplace=True)
+
+    # -------------------------
+    # show some relationships between variables of interest
+    colsOfInterest = ['Volume', 'DailyChange', 'DailyChangeMean',
+                      'DailyChangeStd', 'CloseFutureChange', 'Good']
+
+    sns.pairplot(tickerDf[colsOfInterest])
+
+    # ---------------------------
+    # Visualize the good and bad by 2 features
+    plt.figure()
+    sns.scatterplot(x='DailyChangeMean', y='DailyChangeStd',
+                    hue='Good', data=tickerDf)
+    plt.title(tickerSymbol)
--- models/model1.py
+++ models/model1.py
@@ -1,18 +1,13 @@
 # Notes: basic yfinance demo mostly taken from:
 # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
 
+
 from data import Ticker2
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
+from features import tickerDf  # some data
 
 # sns.set_style('darkgrid')
 sns.set_theme(style='darkgrid', font_scale=1.5)
-
-tickerSymbol = 'MSFT'
-tickerData = Ticker2(tickerSymbol)
-tickerDf = tickerData.history(period='1d', start='2010-1-1', end='2020-1-25')
-
-
-# tickerDf['growthSlope'] = tickerDf['Close'] - tickerDf['Open']
