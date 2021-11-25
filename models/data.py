c9a9bba910767e6ea0cdf1deb8f77af99ba308a3
Samuel Rivera <sriveravi@gmail.com>
2021-11-25 08:19:56 -0700

    now caching and inheriting properly

--- models/data.py
+++ models/data.py
@@ -4,8 +4,8 @@ import yfinance as yf
 
 
 class TickerCached(yf.Ticker):
-    def __init__(self, *args, ticker='MSFT', **kwargs):
-        super().__init__(*args, ticker, **kwargs, )
+    def __init__(self, ticker='MSFT'):
+        super().__init__(ticker)
         saveDir = 'TickerData'
         os.makedirs( saveDir, exist_ok=True)
         self.savePath = os.path.join(saveDir, ticker +'.pkl')
@@ -14,7 +14,17 @@ class TickerCached(yf.Ticker):
         if os.path.exists(self.savePath):
             tickerDf = pd.read_pickle(self.savePath)
         else:
-            # tickerDf = super().history(*args, **kwargs)
-            tickerDf = yf.Ticker(self.ticker).history(*args, **kwargs)
+            tickerDf = super().history(*args, **kwargs)
+            # tickerDf = yf.Ticker(self.ticker).history(*args, **kwargs)
             tickerDf.to_pickle(self.savePath)
         return tickerDf
+
+
+
+
+# get data on this ticker
+tickerSymbol = 'MSFT'
+tickerData = TickerCached(tickerSymbol)
+
+# get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2021-1-1', end='2021-6-25')
